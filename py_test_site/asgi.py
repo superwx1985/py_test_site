7fa5dee5a28ab636ae746442db8a079b1fc799a9
superwx1985 <superwx1985@gmail.com>
2024-02-19 11:36:49 +0800

    修复生产环境启动错误，修复favicon.ico找不到的问题

--- py_test_site/asgi.py
+++ py_test_site/asgi.py
@@ -4,13 +4,14 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 from django.conf import settings
-from main import routing
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "py_test_site.settings")
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
+
+from main import routing  # routing必须在get_asgi_application()后执行
 
 protocol_type_router = {"http": get_asgi_application()}
 # AuthMiddlewareStack 在debug模式时会报错
